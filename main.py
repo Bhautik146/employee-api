281aca979b025209017c0180df65bb9c6171ab0f
Vaghasiya Bhautik <103563924+Bhautikpatel14@users.noreply.github.com>
2026-03-31 16:59:17 +0530

    add psycopg2

--- main.py
+++ main.py
@@ -14,11 +14,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # Database Dependency
 def get_db():
     db = SessionLocal()

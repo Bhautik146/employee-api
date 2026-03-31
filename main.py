b7f5a024e765d1becb242792203919b9cfecf2eb
Vaghasiya Bhautik <103563924+Bhautikpatel14@users.noreply.github.com>
2026-03-31 16:06:56 +0530

    render safe main

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 import hashlib
-import os
 
 from database import SessionLocal, engine
 import models, schemas, crud
@@ -13,16 +10,6 @@ from auth import create_token
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-app.mount(
-    "/static",
-    StaticFiles(directory=os.path.join(BASE_DIR, "static")),
-    name="static"
-)
-
-templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 app.add_middleware(
     CORSMiddleware,
@@ -40,23 +27,11 @@ def get_db():
     finally:
         db.close()
 
-# ------------------ Frontend Pages ------------------
+# ------------------ Root ------------------
 
 @app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
-
-@app.get("/register")
-def register_page(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
-
-@app.get("/login")
-def login_page(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
-
-@app.get("/dashboard")
-def dashboard_page(request: Request):
-    return templates.TemplateResponse("dashboard.html", {"request": request})
+def home():
+    return {"message": "API Running Successfully"}
 
 # ------------------ API Routes ------------------
 
@@ -80,9 +55,11 @@ def login(user: schemas.EmployeeLogin, db: Session = Depends(get_db)):
 
     raise HTTPException(status_code=400, detail="Invalid Login")
 
+
 @app.get("/employees")
 def employees(db: Session = Depends(get_db)):
     return crud.get_all_employees(db)
+
 
 @app.get("/employee/{id}")
 def get_employee(id: int, db: Session = Depends(get_db)):
@@ -92,6 +69,7 @@ def get_employee(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Employee not found")
 
     return emp
+
 
 @app.put("/employee/{id}")
 def update_employee(id: int, employee: schemas.EmployeeCreate, db: Session = Depends(get_db)):
@@ -111,6 +89,7 @@ def update_employee(id: int, employee: schemas.EmployeeCreate, db: Session = Dep
     db.refresh(emp)
 
     return {"message": "Updated successfully"}
+
 
 @app.delete("/employee/{id}")
 def delete_employee(id: int, db: Session = Depends(get_db)):

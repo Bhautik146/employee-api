7b475e4589e68f00a4bb8a523cb5ac871bd6ce85
Vaghasiya Bhautik <103563924+Bhautikpatel14@users.noreply.github.com>
2026-04-01 14:29:27 +0530

    database final fix

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv(
@@ -8,10 +7,18 @@ DATABASE_URL = os.getenv(
     "postgresql://postgres:123@localhost:5432/employee_db"
 )
 
+# Fix Render postgres URL
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(DATABASE_URL)
+# SSL only for Render
+if "render.com" in DATABASE_URL:
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"sslmode": "require"}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,

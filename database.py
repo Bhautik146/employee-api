f229067a34e53b1d51d88e968951173aaa965017
Vaghasiya Bhautik <103563924+Bhautikpatel14@users.noreply.github.com>
2026-04-01 14:03:20 +0530

    database final fix

--- database.py
+++ database.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./employees.db"
-
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:123@localhost:5432/employee_db"
 )
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
